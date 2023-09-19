1d9d8f1ae12bab71de246b8b8ec0f70e26ff3169
Hacker-butcher <aviralg0@gmail.com>
2023-09-19 15:20:37 +0530

    moonlight effect updated

--- 04_neopixel_moonlight/main.py
+++ 04_neopixel_moonlight/main.py
@@ -1,40 +1,26 @@
-#micropython script
-#aviral
-
-#controlling the neopixel led
-
 import machine
 import neopixel
 import time
 
-#object for neopixel
-pixels=neopixel.NeoPixel(machine.Pin(48),1)
+# Initialize the NeoPixel LED
+np = neopixel.NeoPixel(machine.Pin(48), 1)
 
-
+# Function to simulate the moonlight effect
 def moonlight_effect():
     for i in range(256):  # Gradually increase brightness
-        for pixel in pixels:
-            pixel[0] = i  # Set the red component
-            pixel[1] = i  # Set the green component
-            pixel[2] = i  # Set the blue component
-        pixels.write()
-        time.sleep(0.01)  # Adjust the speed of the effect
+        np[0] = (0, 0, i)  # Set the color to blue with increasing intensity (R, G, B)
+        np.write()
+        time.sleep(0.1)  # Adjust the speed of the effect
 
     for i in range(255, -1, -1):  # Gradually decrease brightness
-        for pixel in pixels:
-            pixel[0] = i
-            pixel[1] = i
-            pixel[2] = i
-        pixels.write()
-        time.sleep(0.01)
+        np[0] = (0, 0, i)
+        np.write()
+        time.sleep(0.1)
+
+# Main loop
 while True:
     try:
         moonlight_effect()
-
     except KeyboardInterrupt:
         print("Exit")
         break
-    
-        
-        
-        
