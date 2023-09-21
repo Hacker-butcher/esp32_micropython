53334db2d6999079e04d28ef7efebd0e24889e34
Hacker-butcher <aviralg0@gmail.com>
2023-09-21 16:20:44 +0530

    PWD fading

--- 04_neopixel_moonlight/main.py
+++ 04_neopixel_moonlight/main.py
@@ -8,14 +8,14 @@ np = neopixel.NeoPixel(machine.Pin(48), 1)
 # Function to simulate the moonlight effect
 def moonlight_effect():
     for i in range(256):  # Gradually increase brightness
-        np[0] = (0, 0, i)  # Set the color to blue with increasing intensity (R, G, B)
+        np[0] = (150, 0, i)  # Set the color to blue with increasing intensity (R, G, B)
         np.write()
-        time.sleep(0.1)  # Adjust the speed of the effect
+        time.sleep_ms(10)  # Adjust the speed of the effect
 
     for i in range(255, -1, -1):  # Gradually decrease brightness
-        np[0] = (0, 0, i)
+        np[0] = (150, 0, i)
         np.write()
-        time.sleep(0.1)
+        time.sleep_ms(10)
 
 # Main loop
 while True:
